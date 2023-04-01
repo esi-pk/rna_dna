d056bf710a27a803b592f3ddc946441c3abaa3ae
unknown <ehsanpichka1@gmail.com>
2023-04-01 12:49:16 +0430

    mishe ya na

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+  map<int, string> sample_map { { -1, "one"}, { 2, "two" } };
+
+  cout << sample_map[-1] << " " << sample_map[2] << endl;
+}
