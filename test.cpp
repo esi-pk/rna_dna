5e8141cc0e39c24055aa3be03ccc18789b2a47bd
unknown <ehsanpichka1@gmail.com>
2023-04-01 13:31:07 +0430

    edit 2

--- test.cpp
+++ test.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main() {
   map<int, string> sample_map { { -1, "one"}, { 2, "two" } };
   cout<<"mowzo shodi";
-
+  //meske shod :|
   cout << sample_map[-1] << " " << sample_map[2] << endl;
 }
